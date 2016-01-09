5ec8aef1eac805ef47ec31942015da5073638188
kinduff <abarcadabra@gmail.com>
2016-01-08 21:01:04 -0600

    Imports v1.0.0

---
+++ README.md
@@ -0,0 +1,56 @@
+# Googleflog
+
+```js
+var googleflog = require('googleflog');
+
+googleflog.translate('Me encanta Googleflog jaja saludos');
+// ** meE eEñcAñTA G0o0oGleEfl0oG jAjA zhAlud0ozh -- <3
+```
+
+## Installation
+This repository provides a library that's distributed by `npm`.
+
+```bash
+$ npm install googleflog
+```
+
+## Support
+This module can be used in:
+- AMD
+- CommonJS
+- Browser
+- Node.js
+
+## Usage
+All the code is documented, but here is a brief usage example.
+
+```js
+var googleflog = require('googleflog');
+
+// default configuration
+googleflog.translate('Me encanta Googleflog jaja saludos', {
+  noHead: false,   // no head on result
+  noTail: false,   // no tail on result
+  noTrails: false, // just the result
+  head: '',        // custom head for result
+  tail: ''         // custom tail for result
+});
+// ** meE eEñcAñTA G0o0oGleEfl0oG jAjA zhAlud0ozh -- <3
+
+// randomized custom head and tails values
+googleflog.headMap = ['¸„.-•~¹°”ˆ˜¨', '8>'];
+googleflog.tailMap = ['¨˜ˆ”°¹~•-.„¸', '<8'];
+
+googleflog.t('ola k ase'); // .t() is an alias for translate
+// ¸„.-•~¹°”ˆ˜¨ 0olA Q AzheE <8
+```
+
+## Test
+Uses `mocha` and `assert` for testing. Use `npm` to run the tests.
+
+```
+$ npm test
+```
+
+## License
+See the [LICENSE](LICENSE.md) file for license rights and limitations (MIT).
---
+++ test.js
@@ -0,0 +1,67 @@
+var assert = require('assert');
+var googleflog = require('./googleflog');
+
+// custom configuration
+googleflog.headMap = ['head'];
+googleflog.tailMap = ['tail'];
+
+// test variables
+var defaults = {
+  inputString: 'Hello World!'
+};
+var expectedResults = { 
+  de_fault: 'head HeEll0o w0orld! tail',
+  empty_options: 'head HeEll0o w0orld! tail',
+  head_tail: 'headHeEll0o w0orld!tail',
+  noHead: 'HeEll0o w0orld! tail',
+  noTail: 'head HeEll0o w0orld!',
+  noTrails: 'HeEll0o w0orld!'
+};
+
+// tests
+describe('googeflog', function () {
+  describe('#translate', function () {
+    it('default - no options', function () {
+      var result = googleflog.translate(defaults.inputString);
+      assert.equal(result, expectedResults.de_fault);
+    });
+
+    it('empty options', function () {
+      var result = googleflog.translate(defaults.inputString, {});
+      assert.equal(result, expectedResults.empty_options);
+    });
+
+    it('custom head tail', function () {
+      var result = googleflog.translate(defaults.inputString,
+      {
+        head: 'head',
+        tail: 'tail'
+      });
+      assert.equal(result, expectedResults.head_tail);
+    });
+
+    it('noHead true', function () {
+      var result = googleflog.translate(defaults.inputString,
+      {
+        noHead: true
+      });
+      assert.equal(result, expectedResults.noHead);
+    });
+
+    it('noTail true', function () {
+      var result = googleflog.translate(defaults.inputString,
+      {
+        noTail: true
+      });
+      assert.equal(result, expectedResults.noTail);
+    });
+
+    it('noTrails true', function () {
+      var result = googleflog.translate(defaults.inputString,
+      {
+        noTrails: true
+      });
+      assert.equal(result, expectedResults.noTrails);
+    });
+  });
+});
